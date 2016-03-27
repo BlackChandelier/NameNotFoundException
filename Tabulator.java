a764e60914757e2b9b57cf87f079d53d9dd222fe
BlackChandelier <budaibori95@gmail.com>
2016-03-28 00:09:38 +0200

    Added files via upload

--- Tabulator.java
+++ Tabulator.java
@@ -5,26 +5,26 @@ package projlab;
  *
  */
 public class Tabulator {
-	private int tabNumber=0; //kezdetben a meghívódó függvény elõtt nincs behúzás
+	private static int tabNumber=0; //kezdetben a meghívódó függvény elõtt nincs behúzás
 	
 	//a metódusokat a meghívódásuk sorrendje szerint húzza be
-	public void tabMethod(){
+	public static void tabMethod(){
 		for(int i=0;i<tabNumber;i++)
 			System.out.print("\t");
 	}
 	
 	//növeli a tabulátorok számát
-	public void increaseTabNumber(){
+	public static void increaseTabNumber(){
 		tabNumber++;
 	}
 	
 	//csökkenti a tabulátorok számát
-	public void decreaseTabNumber(){
+	public static void decreaseTabNumber(){
 		tabNumber--;
 	}
 	
 	//lenullázza a tabulátorok számát
-	public void resetTabNumber(){
+	public static void resetTabNumber(){
 		 tabNumber=0;
 	 }
 }
